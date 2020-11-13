35d2f4aca3b122029d846a483d36a4686a627d9d
zamys <elmusharafs@gmail.com>
2020-11-13 21:10:46 +0100

    fixed auth

--- pws/auth.py
+++ pws/auth.py
@@ -1,12 +1,14 @@
 #imports
 import tweepy
+import json
 
-#De twitter developer API keys
-consumer_key = 
-consumer_secret = 
-access_token = 
-access_token_secret = 
+#Twitter API Keys
+with open('pws/keys.json') as keys:
+    key = json.load(keys)
+consumer_key = key['consumer']
+consumer_secret = key['consumer_secret']
+access_token = key['access_token']
+access_token_secret = key['access_token_secret']
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
-
